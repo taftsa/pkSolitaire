f603a31d24bb6778e3cd8e79d3164bbea8335ed0
taftsa <30739359+taftsa@users.noreply.github.com>
2019-04-05 22:28:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 var reset = 0;
 
 $(document).ready(function(){
-	
+
+});
+
+$(document).on('click', '#output', function(){
+	var el = document.documentElement,
+	rfs = el.requestFullscreen
+      || el.webkitRequestFullScreen
+      || el.mozRequestFullScreen
+      || el.msRequestFullscreen
+      ;
+     rfs.call(el);
 });
 
 $(document).on('click', '#hit', function(){
